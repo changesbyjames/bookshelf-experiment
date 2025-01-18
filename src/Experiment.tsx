1044c7bb827ae9a2739310c616c0e4ba997d4adb
James Williams <jr_williams@me.com>
2025-01-18 22:54:22 +0000

    Commited old changes

--- src/Experiment.tsx
+++ src/Experiment.tsx
@@ -45,32 +45,56 @@ const Hardback: FC<PropsWithChildren<HTMLAttributes<HTMLDivElement>>> = ({
   ...props
 }) => {
   return (
-    <div
-      {...props}
-      style={{
-        boxShadow: "0px 11px 20px 4px rgba(0, 0, 0, 0.20)",
-        ...style,
-      }}
-      className={`
-        relative z-10 aspect-[6/9] h-full rounded-sm bg-[#3E4149] bg-cover
+    <div className="relative z-10 group aspect-[6/9] h-full">
+      <div
+        {...props}
+        style={{
+          boxShadow: "0px 11px 20px 4px rgba(0, 0, 0, 0.20)",
+          transformStyle: "preserve-3d",
+          transformOrigin: "left center",
+          ...style,
+        }}
+        className={`
+        absolute inset-0 rounded-sm bg-[#3E4149] bg-cover z-10
         bg-center bg-no-repeat
+        transition-all
+        book
         ${className}
       `}
-    >
+      >
+        <div
+          style={{
+            background:
+              "linear-gradient(180deg, rgba(255, 255, 255, 0.20) 0%, rgba(255, 255, 255, 0.00) 1.23%, rgba(255, 255, 255, 0.00) 98.46%, rgba(255, 255, 255, 0.20) 100%), linear-gradient(90deg, rgba(0, 0, 0, 0.10) 0%, rgba(0, 0, 0, 0.10) 0.79%, rgba(255, 255, 255, 0.30) 1.36%, rgba(255, 255, 255, 0.10) 1.94%, rgba(255, 255, 255, 0.10) 2.78%, rgba(0, 0, 0, 0.10) 4.01%, rgba(255, 255, 255, 0.10) 5.02%, rgba(255, 255, 255, 0.20) 5.53%, rgba(255, 255, 255, 0.10) 5.94%, rgba(254, 254, 254, 0.00) 6.43%, rgba(255, 255, 255, 0.00) 98.73%, rgba(255, 255, 255, 0.20) 100%)",
+          }}
+          className="absolute inset-0 rounded-sm"
+        />
+        <div
+          style={{
+            backgroundImage: "url('/texture.jpeg')",
+          }}
+          className="absolute inset-0 rounded-sm bg-cover bg-center bg-no-repeat opacity-40 mix-blend-hard-light"
+        />
+        {children}
+      </div>
       <div
         style={{
-          background:
-            "linear-gradient(180deg, rgba(255, 255, 255, 0.20) 0%, rgba(255, 255, 255, 0.00) 1.23%, rgba(255, 255, 255, 0.00) 98.46%, rgba(255, 255, 255, 0.20) 100%), linear-gradient(90deg, rgba(0, 0, 0, 0.10) 0%, rgba(0, 0, 0, 0.10) 0.79%, rgba(255, 255, 255, 0.30) 1.36%, rgba(255, 255, 255, 0.10) 1.94%, rgba(255, 255, 255, 0.10) 2.78%, rgba(0, 0, 0, 0.10) 4.01%, rgba(255, 255, 255, 0.10) 5.02%, rgba(255, 255, 255, 0.20) 5.53%, rgba(255, 255, 255, 0.10) 5.94%, rgba(254, 254, 254, 0.00) 6.43%, rgba(255, 255, 255, 0.00) 98.73%, rgba(255, 255, 255, 0.20) 100%)",
+          transform: "perspective(1000px) rotateY(-15deg) scale(0.99)",
         }}
-        className="absolute inset-0 rounded-sm"
-      />
+        className="z-0 bg-gray-100 absolute transform-all duration-200 inset-0 rounded-sm opacity-0 group-hover:opacity-100"
+      ></div>
       <div
         style={{
-          backgroundImage: "url('/texture.jpeg')",
+          transform: "perspective(1000px) rotateY(-25deg) scale(0.99)",
         }}
-        className="absolute inset-0 rounded-sm bg-cover bg-center bg-no-repeat opacity-40 mix-blend-hard-light"
-      />
-      {children}
+        className="z-0 bg-white absolute transform-all duration-200 inset-0 rounded-sm opacity-0 group-hover:opacity-100"
+      ></div>
+      <div
+        style={{
+          ...style,
+        }}
+        className="-z-10 absolute bg-[#3E4149] inset-0 brightness-90 rounded-sm bg-cover bg-center bg-no-repeat"
+      ></div>
     </div>
   );
 };
